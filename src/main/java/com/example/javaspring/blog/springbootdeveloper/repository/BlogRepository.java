1695d2f0148b0f63cfaa59f7f16f0485ea84600d
min_e <101707509+skdhfel2@users.noreply.github.com>
2024-11-01 17:31:57 +0900

    Update BlogRepository.java

--- src/main/java/com/example/javaspring/blog/springbootdeveloper/repository/BlogRepository.java
+++ src/main/java/com/example/javaspring/blog/springbootdeveloper/repository/BlogRepository.java
@@ -5,3 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BlogRepository extends JpaRepository<Article, Long> {
 }
+// BlogRepository 인터페이스가 JpaRepository 인터페이스를 확장
+// JpaRepository<Article, Long>은 JPA를 사용해 데이터베이스와 상호작용할 수 있는 여러 CRUD 메서드를 자동으로 제공
+// Article은 데이터베이스 테이블에 해당하는 엔티티 클래스이며, Long은 이 테이블의 기본 키 타입을 지정
