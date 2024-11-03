bbe658b4fc7ee680c464ff52546deb1e06e835de
min_e <101707509+skdhfel2@users.noreply.github.com>
2024-11-03 22:49:12 +0900

    Update BlogApiController.java

--- src/main/java/com/example/javaspring/blog/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/com/example/javaspring/blog/springbootdeveloper/controller/BlogApiController.java
@@ -19,10 +19,10 @@ public class BlogApiController {
     @PostMapping("api/articles") // HTTP 메서드가 POST 일 때 전달받은 URL과 동일하면 메서드로 매핑, 웹 애플리케이션 데이터 생성을 위한 요청으로 주로 사용
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
         // ResponseEntity<Article> 타입의 데이터를 요청한 클라이언트에 반환 = Article 객체를 생성해 데이터 베이스에 저장 -> 그 객체를 ResponseEntity에 담아 클라이언트에 반환 -> 클라이언트는 서버에서 생성된 데이터를 받음
-        // addArticle은 새로운 Article을 데이터베이스에 저장하는 로직을 정의 ->
+        // addArticle은 새로운 Article을 데이터베이스에 저장하는 로직을 정의 -> 그 결과를 HTTP(클라이언트와 서버 간에 데이터를 전송하는데 사용되는 프로토콜(규칙)) 응답으로 반환하는 역할을 한다.
+        // @RequestBody AddArticleRequest request는 HTTP 요청의 본문에 있는 JSON(JavaScript Object Notation)(데이터 형식을 저장하고 전송하는 데 사용, 텍스트 기반의 형식) 데이터를 addArticleRequest 타입의 객체로 변환해 request 변수에 담는다.
         Article savedArticle = blogService.save(request);
 
         return ResponseEntity.status((HttpStatus.CREATED))
                 .body(savedArticle);
     }
-}
