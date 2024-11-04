bad4b72ca0cb04cc4c825d3e1563ce0538688925
min_e <101707509+skdhfel2@users.noreply.github.com>
2024-11-04 13:16:48 +0900

    Update Article.java

--- src/main/java/com/example/javaspring/blog/springbootdeveloper/domain/Article.java
+++ src/main/java/com/example/javaspring/blog/springbootdeveloper/domain/Article.java
@@ -6,13 +6,18 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+// 필드 == 변수
 @Entity // 엔티티로 지정 : 데이터베이스의 레코드 = 테이블의 한 행(row), CRUD(생성,읽기,업데이트,삭제) 작업의 기본 단위로 활용
 @Getter // 게터 메서드를 정의 하지 않기 위해서 사용
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자를 정의하지 않아도 됨, JPA 엔티티에서 기본 생성자를 외부에서 직접 호출할 수 없게 보호하는 용도
 public class Article {
 
     @Id // id 필드가 엔티티의 기본 키임을 나타냄, 기본키 : 테이블에서 각 레코드를 고유하게 식별
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // 데이터베이스에서 기본 키 값을 자동으로 생성하는 방식을 정의, 기본키를 자동으로 1씩 증가
+    @GeneratedValue(strategy = GenerationType.IDENTITY ) // 기본 키 값을 일일히 설정할 필요없이 데이터베이스에서 기본 키 값을 자동으로 생성하게 만드는 역할
+    // @GeneratedValue: 기본 키값을 자동으로 생성
+    // strategy 속성은 기본 키 생성 전략(새로운 기본 키 값을 어떻게 만들지 정하는 규칙)을 정의
+    // GenerationType.IDENTITY: IDENTITY 전략은 데이터베이스가 자동으로 고유한 값을 생성하고 관리하게 만들고 새로운 데이터가 추가될 때마다 기본 키 값이 자동으로 1씩 증가한다.
+    // Article 엔티티에 id가 기본 키로 설정된 상태이므로 데이터베이스가 각 Article 레코드에 대해 고유한 id 값을 자동으로 할당한다.
     @Column(name = "id" , updatable = false) // @Column(name = "id") : "id"라는 변수가 데이터베이스의 id 칼럼과 연결, updatable = false : 한번 설정된 이후에는 수정 불가
     private Long id; // 외부에서 접근할수 없는 long 타입 id 필드(=변수) 정의
 
